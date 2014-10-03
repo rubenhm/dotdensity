d1a444d2a1733f2233e076c146a80ba9df08fda5
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-10-03 10:42:43 -0700

    Color ramp finalized

--- dotdensity.js
+++ dotdensity.js
@@ -31,36 +31,47 @@ require([
     center: [-121.9, 38.6],
     zoom: 10,
     maxZoom: 12,
-    minZoom: 9
+    minZoom: 10
   });
   
   var basemap = new ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer");
   map.addLayer(basemap);
 
   var layer = new FeatureLayer("https://darcgis.water.ca.gov/arcgis/rest/services/cadre/yolo_tracts_by_race/MapServer/0", {
+    mode: FeatureLayer.MODE_SNAPSHOT,
     outFields: ["tract","White","Hispanic","Asian","Black","Pacific_Islander","American_Indian"]
   });
+
+  var dotSizes = {80:4,20:3,5:3}
   
   var createRenderer = function(dotValue) {
     return new DotDensityRenderer({
       fields: [{
         name: "White",
-        color: new Color([21, 137, 255])
+        color: new Color("#a6cee3")
       }, {
         name: "Hispanic",
-        color: new Color([200, 111, 0])
+        color: new Color("#1f78b4")
       }, {
         name: "Asian",
-        color: new Color([0, 255, 0])
+        color: new Color("#33a02c")
       }, {
         name: "Black",
-        color: new Color([255, 0, 0])
+        color: new Color("#e41a1c")
+      },
+      {
+        name: "Pacific_Islander",
+        color: new Color("#984ea3")
+      },
+      {
+        name: "American_Indian",
+        color: new Color("#e7298a")
       }],
       dotValue: dotValue,
-      dotSize: 2,
+      dotSize: dotSizes[dotValue],
       outline: new SimpleLineSymbol("solid", new Color([50, 50, 50, 1]), 0.5),
       legendOptions: {
-        valueUnit: "people",
+        valueUnit: "mortgages",
         backgroundColor: new Color([32, 32, 32])
       }
     });
@@ -68,19 +79,15 @@ require([
   
   var rendererInfos = [
     {
-      renderer: createRenderer(400),
-      maxZoom: 9,
-      minZoom: 9
-    }, {
-      renderer: createRenderer(100),
+      renderer: createRenderer(80),
       maxZoom: 10,
       minZoom: 10
     }, {
-      renderer: createRenderer(25),
+      renderer: createRenderer(20),
       maxZoom: 11,
       minZoom: 11
     }, {
-      renderer: createRenderer(6),
+      renderer: createRenderer(5),
       maxZoom: 12,
       minZoom: 12
     }
